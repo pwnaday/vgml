cd1317765d3cefcf0943872e4d2194b86b0f65ef
pwn <pwnaday@github.com>
2020-08-20 21:22:18 -0500

    !

--- vgml.py
+++ vgml.py
@@ -21,6 +21,21 @@ import os
 import gizeh as gz
 from math import pi
 
+""" Config """
+dataroot = os.getcwd() + "/data"
+workers = 2
+batch_size = 128
+image_size = 64
+nc = 3
+nz = 29
+ngf = 64
+ndf = 64
+num_epochs = 5
+lr = 0.0002
+beta = 0.5
+npgu = 1
+""""""
+
 def make_yinyang(size=200, r=80, filename="yin_yang.png"):
     surface = gz.Surface(size, size, bg_color=(0, .3 , .6))
     yin_yang = gz.Group([
@@ -38,4 +53,17 @@ def main() :
     seed = 999
     random.seed(seed)
     torch.manual_seed(seed)
-
+    dataset = dset.ImageFolder(root=dataroot, transform=transforms.Compose([\
+					transforms.Resize(image_size),\
+					transforms.CenterCrop(image_size),\
+					transforms.ToTensor(),\
+					transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))\
+			    ]))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
+    device = torch.device('cuda:0' if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("training imgs")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(), (1,2,0)))
+main()
